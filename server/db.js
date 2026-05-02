4ef77077255cf795dc45961e2b2df4696657b0fc
gayathri-2206 <napagayathri1@gmail.com>
2026-05-02 13:26:26 +0530

    commit19

--- server/db.js
+++ server/db.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-
+console.log("ENV CHECK:");
+console.log("HOST:", process.env.DB_HOST);
+console.log("USER:", process.env.DB_USER);
+console.log("DB:", process.env.DB_NAME);
+console.log("PORT:", process.env.DB_PORT);
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -9,11 +13,17 @@ const db = mysql.createConnection({
   port: 3306
 });
 
-db.connect(err=>{
-  if(err) console.log(err);
-  else console.log("MySQL Connected");
+// db.connect(err=>{
+//   if(err) console.log(err);
+//   else console.log("MySQL Connected");
+// });
+db.connect(err => {
+  if (err) {
+    console.log("DB ERROR:", err);
+  } else {
+    console.log("MySQL Connected ✅");
+  }
 });
-
 module.exports = db;
 
 
