a3d8b020272c02e38ef9a7f0254ab70521938a97
gayathri-2206 <napagayathri1@gmail.com>
2026-05-02 11:53:49 +0530

    commit10

--- server/server.js
+++ server/server.js
@@ -3,22 +3,21 @@ const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
 
-const app = express(); // ✅ FIRST create app
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// API routes
+// routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, "../client")));
+// serve frontend
+app.use(express.static(path.join(__dirname, "client")));
 
-// Default route (important)
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "client/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
