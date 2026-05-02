ad99f83f177c1e627c1ec8ec5691a2079bbcb93f
gayathri-2206 <napagayathri1@gmail.com>
2026-05-02 11:33:15 +0530

    commit6
    commit

--- server/server.js
+++ server/server.js
@@ -1,18 +1,23 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path"); // ✅ ADD THIS
+require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
-app.use(express.static(path.join(__dirname, "../client")));
+
+// serve frontend
+app.use(express.static(path.join(__dirname, "../client"))); // now works
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log("Server running");
 });
